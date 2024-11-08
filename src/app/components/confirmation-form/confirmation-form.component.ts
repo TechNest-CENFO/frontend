8e7a69fc5f2e184b24d0d7b09034bbc97024e44f
n3land <nelsonvc89@gmail.com>
2024-11-08 12:28:09 -0600

    technest-49: al eliminar cuenta hace logout y redirige al login

--- src/app/components/confirmation-form/confirmation-form.component.ts
+++ src/app/components/confirmation-form/confirmation-form.component.ts
@@ -3,6 +3,9 @@ import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angul
 import { IUser } from '../../interfaces';
 import { CommonModule } from '@angular/common';
 import { ProfileService } from '../../services/profile.service';
+import { ModalService } from '../../services/modal.service';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-confirmation-form',
@@ -15,11 +18,16 @@ import { ProfileService } from '../../services/profile.service';
   styleUrl: './confirmation-form.component.scss'
 })
 export class ConfirmationFormComponent {
+
   @Input() confirmationForm!: FormGroup;
   public profileService: ProfileService = inject(ProfileService);
+  public modalService: ModalService = inject(ModalService);
+  public authService: AuthService = inject(AuthService);
 
+  constructor(private router: Router
+  ) { }
 
-  deleteAccount(event: Event){
+  deleteAccount(){
     let user = {
       isUserActive: false
     }
@@ -28,5 +36,13 @@ export class ConfirmationFormComponent {
 
   deleteUser(user: IUser){
     this.profileService.deleteUser(user);
+    this.closeModal();
+    this.authService.logout();
+    this.router.navigate(['/login']); 
+
+  }
+
+  closeModal(){
+    this.modalService.closeAll();
   }
 }
