27e2f179ebe9c49a96d5e47db40b8ecaff0118cf
n3land <nelsonvc89@gmail.com>
2024-11-10 16:03:56 -0600

    technest-14: valida el path relativo para determinar si utiliza el apiUrl

--- src/app/interceptors/base-url.interceptor.ts
+++ src/app/interceptors/base-url.interceptor.ts
@@ -3,9 +3,11 @@ import { environment } from '../../environments/environment';
 
 export const baseUrlInterceptor: HttpInterceptorFn = (req, next) => {
   const base: string = environment.apiUrl;
+  const isRelativeUrl = !req.url.startsWith('http');
+  const url = isRelativeUrl ? `${base}/${req.url}` : req.url;
 
   const clonedRequest = req.clone({
-    url: `${base}/${req.url}`,
+    url,
     setHeaders: {
       Accept: 'application/json',
     },
