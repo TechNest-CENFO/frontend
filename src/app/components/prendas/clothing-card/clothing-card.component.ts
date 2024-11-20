59412e50347f9cf6c5fc971ce521851bc0fae62b
n3land <nelsonvc89@gmail.com>
2024-11-20 13:05:54 -0600

    technest-37: incluye funcionalidad de editar prenda

--- src/app/components/prendas/clothing-card/clothing-card.component.ts
+++ src/app/components/prendas/clothing-card/clothing-card.component.ts
@@ -1,9 +1,10 @@
 import {Component, EventEmitter, inject, Input, Output} from '@angular/core';
-import {IClothing} from "../../../interfaces";
+import {IClothing, IClothingType} from "../../../interfaces";
 import { ModalService } from '../../../services/modal.service';
 import { ModalComponent } from '../../modal/modal.component';
 import { ClothingEditComponent } from '../clothing-edit/clothing-edit.component';
 import { ClothingDeleteConfirmationComponent } from '../clothing-delete-confirmation/clothing-delete-confirmation.component';
+import { ClothingTypeService } from '../../../services/clothing-type.service';
 @Component({
   selector: 'app-clothing-card',
   standalone: true,
@@ -15,7 +16,8 @@ export class ClothingCardComponent {
   @Input() clothing!: IClothing;
   @Output() callEditAction: EventEmitter<IClothing> = new EventEmitter<IClothing>();
   public modalService: ModalService = inject(ModalService);
-
+  public clothingTypeService: ClothingTypeService = inject(ClothingTypeService);
+  clothingTypeData: IClothingType[] = []; // Almacenar los datos de los tipos de prendas
 
   isFav: boolean = false;
   toggleIsFav() {
@@ -25,5 +27,20 @@ export class ClothingCardComponent {
 
   onEdit() {
     this.callEditAction.emit(this.clothing);
+  }
+
+
+
+  getAllTypeClothing(): void {
+    // Llamamos a getAll() y al Observable para obtener los datos
+    this.clothingTypeService.getAll().subscribe({
+        next: (response) => {
+            // Accedemos a los datos y los almacenamos en la propiedad clothingData
+            this.clothingTypeData = response.data;
+        },
+        error: (err) => {
+            err = "Ocurrió un error al cargar los datos.";
+        }
+    });
   }
 }
